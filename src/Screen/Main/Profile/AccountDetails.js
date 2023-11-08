91c0d638860b1836de6b33534d3c213ae98eb31b
anhnq11 <anhnqcontact@gmail.com>
2023-11-09 01:21:52 +0700

    Update User Info

--- src/Screen/Main/Profile/AccountDetails.js
+++ src/Screen/Main/Profile/AccountDetails.js
@@ -1,11 +1,88 @@
-import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Image, TextInput, ToastAndroid } from 'react-native'
 import React from 'react'
 import { Icon } from '@rneui/themed'
-import { useSelector } from 'react-redux'
-import { selectUser } from '../../../Redux/Reducer/Reducer'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { login, selectUser } from '../../../Redux/Reducer/Reducer'
+import * as ImagePicker from 'expo-image-picker';
+import URL from '../../../UrlApi'
+import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { isEmail, isPhoneNumber } from '../../../validate'
 
 const AccountDetails = ({ navigation }) => {
+
     const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+    const [image, setImage] = useState(user.image)
+    const [fullname, setFullname] = useState(user.fullname)
+    const [phonenum, setPhonenum] = useState(user.phonenum)
+    const [email, setEmail] = useState(user.email)
+    const [error, setError] = useState(null)
+
+    const chooseImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    }
+
+    const handleUpdate = async () => {
+
+        setError(null);
+        
+        if(!fullname || !phonenum || !email){
+            setError('Vui lòng điền đầy đủ thông tin!')
+            return;
+        }
+
+        if ( !isEmail(email) || !isPhoneNumber(phonenum) ){
+            setError('Số điện thoại hoặc Email không đúng định dạng!')
+            return;
+        }
+
+        const data = {
+            _id: user._id,
+            fullname: fullname,
+            phonenum: phonenum,
+            email: email,
+            image: image
+        }
+
+        await axios({
+            method: 'post',
+            url: `${URL}users/update`,
+            data: data
+        })
+            .then((res) => {
+                if (res.status == 200) {
+                    dispatch(login(res.data))
+                    saveUserData(res.data)
+                    ToastAndroid.show('Cập nhật thông tin thành công!', ToastAndroid.SHORT)
+                    navigation.goBack();
+                    return;
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    // Lưu thông tin người dùng khi đăng nhập thành công
+    const saveUserData = async (userData) => {
+        try {
+            await AsyncStorage.setItem('userData', JSON.stringify(userData));
+        } catch (error) {
+            console.error('Lỗi khi lưu thông tin người dùng:', error);
+        }
+    };
+
     return (
         <View style={{
             width: '100%',
@@ -31,41 +108,107 @@ const AccountDetails = ({ navigation }) => {
                     textAlign: 'center'
                 }}>Trang cá nhân</Text>
             </View>
-            <View style={{
-                    width: 100,
-                    height: 100,
-                    borderRadius: 100,
-                    overflow: 'hidden',
-                    marginTop: 30
-                }}>
+            <View>
+                <Image
+                    source={{ uri: image ? image : "https://th.bing.com/th/id/R.55393befef8a75d3a59d25ee7931d60f?rik=Dmwko1HbUCWZ7w&riu=http%3a%2f%2fgetdrawings.com%2ffree-icon%2fpowerpoint-user-icon-59.png&ehk=zkEtxliUAs%2fFgrsCJuqa4qWXNltwnt8b9%2fyMoGmWSYU%3d&risl=&pid=ImgRaw&r=0" }}
+                    style={{
+                        width: 100,
+                        height: 100,
+                        borderRadius: 100,
+                        marginTop: 30
+                    }}
+                />
+                <TouchableOpacity onPress={() => chooseImage()}>
                     <Image
-                        source={{ uri: user.image }}
+                        source={{ uri: "https://cdn1.iconfinder.com/data/icons/user-fill-icons-set/144/User003_Edit-512.png" }}
                         style={{
-                            width: '100%',
-                            height: '100%',
-                            resizeMode: 'cover'
+                            width: 35,
+                            height: 35,
+                            tintColor: '#EFE3C8',
+                            position: 'absolute',
+                            bottom: -5,
+                            right: -5,
                         }}
                     />
-                </View>
+                </TouchableOpacity>
+            </View>
             <Text style={{
+                width: '100%',
                 color: '#EFE3C8',
-                fontSize: 30,
+                fontSize: 22,
                 marginTop: 10,
                 fontWeight: 'bold'
             }}>
-                {user.fullname}
+                Thông tin cá nhân
             </Text>
+            {
+                error ? (
             <Text style={{
-                color: '#EFE3C8',
-                fontSize: 20,
-                marginTop: 10
+                width: '100%',
+                color: 'red',
+                fontSize: 17,
+                marginTop: 10,
+                fontStyle: 'italic'
             }}>
-                {user.email}
+                *{ error }
             </Text>
+                ) : (<View></View>)
+            }
+            <TextInput
+                type="text"
+                placeholder='Họ và tên...'
+                value={fullname}
+                placeholderTextColor={'#EFE3C8'}
+                style={styles.input}
+                onChangeText={(text) => { setFullname(text) }} />
+            <TextInput
+                type="text"
+                placeholder='Số điện thoại...'
+                value={phonenum}
+                placeholderTextColor={'#EFE3C8'}
+                style={styles.input}
+                onChangeText={(text) => { setPhonenum(text) }} />
+            <TextInput
+                type="text"
+                placeholder='Email...'
+                value={email}
+                placeholderTextColor={'#EFE3C8'}
+                style={styles.input}
+                onChangeText={(text) => { setEmail(text) }} />
+            <TouchableOpacity style={styles.button} onPress={() => { handleUpdate() }}>
+                <Text style={styles.text}>Cập nhật</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 export default AccountDetails
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    input: {
+        width: '100%',
+        borderRadius: 20,
+        borderColor: '#EFE3C8',
+        color: '#EFE3C8',
+        borderWidth: 2,
+        marginTop: 10,
+        fontSize: 20,
+        paddingVertical: 5,
+        paddingHorizontal: 20,
+    },
+    button: {
+        width: '100%',
+        backgroundColor: '#EFE3C8',
+        marginTop: 10,
+        borderRadius: 20,
+        paddingVertical: 1,
+        borderColor: '#EFE3C8',
+        borderWidth: 2,
+    },
+    text: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 25,
+        color: '#201520'
+    },
+})
--- src/validate.js
+++ src/validate.js
@@ -1,2 +1,7 @@
+export const isEmail = (email) =>
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email,
+  );
+
 export const isPhoneNumber = (phoneNumber) =>
   /(0[3|5|7|8|9])+([0-9]{8})\b/.test(phoneNumber);
